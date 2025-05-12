6c9d5c702ce6b0936e91060c2cfeb87549fa1de5
yaruru0214 <yaruru0214@gmail.com>
2025-05-12 17:34:47 +0900

    이상해졌던거 다시 세팅함

---
+++ Source/Project_DT/Private/Familiar/CFamiliarBase.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Familiar/CFamiliarBase.h"
+
+// Sets default values
+ACFamiliarBase::ACFamiliarBase()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACFamiliarBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACFamiliarBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
---
+++ Source/Project_DT/Public/Familiar/CFamiliarWolf.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Familiar/CFamiliarBase.h"
+#include "CFamiliarWolf.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_DT_API ACFamiliarWolf : public ACFamiliarBase
+{
+	GENERATED_BODY()
+	
+};
