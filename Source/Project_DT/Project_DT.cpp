2aa60111d82e06d092a6d5a8719a863fb0537ba1
wonhwo <reppy0813@gmail.com>
2025-05-12 14:42:05 +0900

    프로젝트 초기 설정

---
+++ Source/Project_DT/Project_DT.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project_DT.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_DT, "Project_DT" );
---
+++ Source/Project_DT/Public/Global.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "DrawDebugHelpers.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/KismetMathLibrary.h"
+
+#include "Utilities/CHelper.h"
+#include "Utilities/CLog.h"
